a4bb44cddfad503884f71cbd65aa80ef27a1adde
Fred TU <frederic.tu@gmail.com>
2017-08-26 23:26:55 +0800

    Add Hortonworks Schema Registry docker utilities

---
+++ docker-schema-registry.bash
@@ -0,0 +1,20 @@
+echo "== Type 'inst_script' to install bash-fwk locally =="
+
+inst_script() {
+	echo "Install bash-fwk that contains Docker & Vagrant utilities."
+	echo "$(curl -fsSL https://raw.githubusercontent.com/frtu/bash-fwk/master/autoinstaller4curl.bash)" | bash
+}
+
+# MANUAL Docker build : https://github.com/hortonworks/registry/blob/master/docker/images/registry/Dockerfile
+#
+# or Leverage thebookpeople/hortonworks-registry to avoid building manually. Thanks to TheBookPeople.
+# https://github.com/TheBookPeople/hortonworks-registry-docker
+# https://hub.docker.com/r/thebookpeople/hortonworks-registry/
+
+echo "== Type 'schemaregistry_hortonworks_start' to start Hortonworks Schema Registry =="
+
+schemaregistry_hortonworks_start() {
+	echo "Make sure you have ** docker-compose ** installed !!"
+	(cd docker/hortonworks-schema-registry && exec docker-compose up)
+	dckmport 9090
+}
