75edce8b035b43c6da2a7eb93cbd851e9840c8d6
Fred <frederic.tu@gmail.com>
2018-10-21 13:15:21 +0800

    Add sample Starter class that list all files in the test directory Logback & log properties

---
+++ archetype/spark-project/src/main/resources/archetype-resources/__rootArtifactId__/src/main/scala/Starter.scala
@@ -0,0 +1,31 @@
+package ${groupId}
+
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Starter {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("${artifactId}")
+
+    // MODE LOCAL or YARN
+    if (args.isEmpty) sparkConf.setMaster("local[*]")
+
+    // INPUT FILE : Put your test data file here (CSV, ...)
+    // => You can get some sample data at : https://support.spatialkey.com/spatialkey-sample-csv-data/
+    var data = if (!args.isEmpty) args(0) else "${artifactId}/src/test/resources/"
+
+    val sparkContext: SparkContext = new SparkContext(sparkConf)
+    val system = FileSystem.get(sparkContext.hadoopConfiguration)
+
+    val files: Seq[String] = for {
+      folder <- system.listStatus(new Path(data));
+      file <- system.listStatus(folder.getPath)
+    } yield file.getPath.toString
+
+    files.foreach(sourceFile => {
+      println("PATH:" + sourceFile)
+    })
+
+    sparkContext.stop
+  }
+}
