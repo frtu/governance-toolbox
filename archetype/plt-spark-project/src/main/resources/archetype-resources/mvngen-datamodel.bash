c7979e0d2354294697aff714fc2591014b4c71d4
Fred <frederic.tu@gmail.com>
2018-10-21 20:54:47 +0800

    Rename and create a plt-spark archetype project with a bash cmd to create avro data model project

---
+++ archetype/plt-spark-project/src/main/resources/archetype-resources/mvngen-datamodel.bash
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Generate model with > gendatamodel DATA_MODEL_PROJECT_NAME"
+gendatamodel() {
+  # MIN NUM OF ARG
+  if [[ "$#" < "1" ]]; then
+    echo "Usage : ${FUNCNAME[0]} DATA_MODEL_PROJECT_NAME" >&2
+    return -1
+  fi
+
+  local DATA_MODEL_PROJECT_NAME=$1
+
+  echo "mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=avro-project-archetype -DarchetypeVersion=${base-pom-version} -DgroupId=${groupId} -DartifactId=${DATA_MODEL_PROJECT_NAME} -Dversion=${version}"
+  mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=avro-project-archetype -DarchetypeVersion=${base-pom-version} -DgroupId=${groupId} -DartifactId=${DATA_MODEL_PROJECT_NAME} -Dversion=${version}
+}
