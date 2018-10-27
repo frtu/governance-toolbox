d3040e9c5c217ee87d5ca0bcafed4505632a108c
Fred <frederic.tu@gmail.com>
2018-10-27 22:44:03 +0800

    Allow to start Spark application with "mvn exec:java"

--- archetype/plt-spark-project/src/main/resources/archetype-resources/mvn-cmds.bash
+++ archetype/plt-spark-project/src/main/resources/archetype-resources/mvn-cmds.bash
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo "Generate model with > gendatamodel DATA_MODEL_PROJECT_NAME"
+echo "== Type 'runspark' to run the inner project Spark application =="
+runspark() {
+	echo "Running Spark application : ${artifactId}"
+	(cd ${artifactId} && mvn clean compile exec:java)
+}
+
+echo "== Generate model with > gendatamodel DATA_MODEL_PROJECT_NAME =="
 gendatamodel() {
   # MIN NUM OF ARG
   if [[ "$#" < "1" ]]; then
@@ -13,3 +19,4 @@ gendatamodel() {
   echo "mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=avro-project-archetype -DarchetypeVersion=${base-pom-version} -DgroupId=${groupId} -DartifactId=${DATA_MODEL_PROJECT_NAME} -Dversion=${version}"
   mvn archetype:generate -DarchetypeGroupId=com.github.frtu.archetype -DarchetypeArtifactId=avro-project-archetype -DarchetypeVersion=${base-pom-version} -DgroupId=${groupId} -DartifactId=${DATA_MODEL_PROJECT_NAME} -Dversion=${version}
 }
+
