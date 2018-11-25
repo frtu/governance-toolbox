c1983c06709c871ee3e0456254690e44e319eecd
Fred <frederic.tu@gmail.com>
2018-11-25 22:14:01 +0800

    Add one test sample

--- archetype/plt-spark-project/src/main/resources/archetype-resources/__rootArtifactId__/src/test/scala/StarterSpec.scala
+++ archetype/plt-spark-project/src/main/resources/archetype-resources/__rootArtifactId__/src/test/scala/StarterSpec.scala
@@ -2,9 +2,15 @@ package ${groupId}
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.FlatSpec
+import scala.io.Source
+import java.io.{File => JFile}
 
-object StarterSpec extends FlatSpec {
+class StarterSpec extends FlatSpec {
   private val sc = new SparkContext(new SparkConf().setMaster("local[2]").setAppName(this.getClass.getCanonicalName).set("spark.ui.enabled", "false"))
   //  http://www.scalatest.org/user_guide/writing_your_first_test
 
+  "main" should "return find the file with one line" in {
+    val lines = Source.fromFile(new JFile("src/test/resources/simple.txt")).getLines()
+    assert(lines.length == 1)
+  }
 }
