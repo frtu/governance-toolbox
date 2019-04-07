61126d371f4fabd75bca47346e92bb983d2f9acd
Tu, Fred <frederic.tu@gmail.com>
2019-04-07 23:57:51 +0800

    Creating one level StructType

--- schema-registries/schema-maven-plugin/src/main/java/com/github/frtu/schema/utils/Avro2StructTypeGenerator.java
+++ schema-registries/schema-maven-plugin/src/main/java/com/github/frtu/schema/utils/Avro2StructTypeGenerator.java
@@ -5,7 +5,9 @@ import org.apache.spark.sql.types.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Allow to generate a StructType from an Avro schema
@@ -30,7 +32,40 @@ public class Avro2StructTypeGenerator {
     }
 
     public String generateStructType(Schema avroSchema) {
-        final StructType structType = new StructType();
+        final String namespace = avroSchema.getNamespace();
+        final String schemaName = avroSchema.getName();
+        LOGGER.info("Mapping schema namespace={} schemaName={}", namespace, schemaName);
+
+        final List<StructField> structFields = new ArrayList<>();
+        final List<Schema.Field> fields = avroSchema.getFields();
+
+        fields.forEach(field -> {
+            final String fieldName = field.name();
+            boolean isNullable = false;
+
+            final Schema schema = field.schema();
+            final Schema.Type type = schema.getType();
+
+            DataType dataType = typeMappingToShape.get(type);
+            if (dataType == null) {
+                if (Schema.Type.UNION.equals(type)) {
+                    for (Schema schemaItem : schema.getTypes()) {
+                        final Schema.Type schemaItemType = schemaItem.getType();
+
+                        if (Schema.Type.NULL.equals(schemaItemType)) {
+                            isNullable = true;
+                        } else {
+                            dataType = typeMappingToShape.get(schemaItemType);
+                        }
+                    }
+                }
+            }
+            final StructField structField = new StructField(fieldName, dataType, isNullable, Metadata.empty());
+            structField.withComment(field.doc());
+            structFields.add(structField);
+        });
+
+        final StructType structType = new StructType(structFields.toArray(new StructField[structFields.size()]));
         return structType.toString();
     }
 }
--- schema-registries/schema-maven-plugin/src/test/java/com/github/frtu/schema/utils/Avro2StructTypeGeneratorTest.java
+++ schema-registries/schema-maven-plugin/src/test/java/com/github/frtu/schema/utils/Avro2StructTypeGeneratorTest.java
@@ -2,6 +2,7 @@ package com.github.frtu.schema.utils;
 
 import org.apache.avro.Schema;
 import org.junit.Test;
+import tests.pojo.complex.ComplexStructureLowerChild;
 import tests.pojo.complex.ComplexStructureParent;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,8 @@ import static org.junit.Assert.*;
 public class Avro2StructTypeGeneratorTest {
 
     @Test
-    public void generateDot() {
-        final Schema avroSchema = SchemaUtil.genAvroSchemaFrom(ComplexStructureParent.class);
+    public void generateStructType() {
+        final Schema avroSchema = SchemaUtil.genAvroSchemaFrom(ComplexStructureLowerChild.class);
         System.out.println(avroSchema.toString(true));
 
         final Avro2StructTypeGenerator avro2StructTypeGenerator = new Avro2StructTypeGenerator();
