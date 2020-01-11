2ac7ff14d910433c7f4764569274830be66925f5
Tu, Fred <frederic.tu@gmail.com>
2020-01-12 01:33:47 +0800

    Add usage for SecuredFieldHelper

--- ext-protobuf/archetype-proto-project/src/main/resources/archetype-resources/src/test/java/SampleTest.java
+++ ext-protobuf/archetype-proto-project/src/main/resources/archetype-resources/src/test/java/SampleTest.java
@@ -1,6 +1,14 @@
 package ${groupId};
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.github.frtu.proto.metadata.SecuredFieldHelper;
+import com.github.frtu.proto.metadata.utils.ProtoUtil;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
+import java.util.Map;
 
 public class SampleTest {
     @Test
@@ -15,5 +23,16 @@ public class SampleTest {
                 .includingDefaultValueFields()
                 .print(sample);
         System.out.println(json);
+    }
+
+    @Test
+    public void isSecured() {
+        SecuredFieldHelper securedFieldHelper = new SecuredFieldHelper();
+
+        final Descriptors.Descriptor descriptor = SampleData.getDescriptor();
+        final Map<String, Descriptors.FieldDescriptor> fieldDescriptorMap = ProtoUtil.buildFieldsMap(descriptor);
+
+        assertFalse(securedFieldHelper.isSecured(fieldDescriptorMap.get("name")));
+        assertTrue(securedFieldHelper.isSecured(fieldDescriptorMap.get("value")));
     }
 }
