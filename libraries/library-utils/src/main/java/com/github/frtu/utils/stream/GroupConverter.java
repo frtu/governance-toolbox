3462a2274ced6217876b83995213c404f2f5e659
Tu, Fred <frederic.tu@gmail.com>
2020-01-11 22:32:30 +0800

    Add illustration of stream Collectors toMap() & joining()

---
+++ libraries/library-utils/src/main/java/com/github/frtu/utils/stream/GroupConverter.java
@@ -0,0 +1,74 @@
+package com.github.frtu.utils.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Class illustrating a few {@link java.util.stream.Stream} use cases.
+ */
+public class GroupConverter {
+    /**
+     * Create a Map keyed using internal items
+     *
+     * @param sourceList source list
+     * @param keyMapper  from source type to key
+     * @param <Source>   Source Type
+     * @param <Key>      Key Type
+     * @return converted map
+     */
+    public static <Source, Key> Map<Key, Source> toMapByKey(List<Source> sourceList, Function<Source, Key> keyMapper) {
+        return toMap(sourceList, keyMapper, Function.identity());
+    }
+
+    /**
+     * Create a Map keyed using internal items
+     *
+     * @param sourceList  source list
+     * @param valueMapper from source type to value
+     * @param <Source>    Source Type
+     * @param <Value>     Value Type
+     * @return converted map
+     */
+    public static <Source, Value> Map<Source, Value> toMapByValue(List<Source> sourceList, Function<Source, Value> valueMapper) {
+        return toMap(sourceList, Function.identity(), valueMapper);
+    }
+
+    /**
+     * Create a Map from a List using internal items
+     *
+     * @param sourceList  source list
+     * @param keyMapper   from source type to key
+     * @param valueMapper from source type to value
+     * @param <Source>    Source Type
+     * @param <Key>       Key Type
+     * @param <Value>     Value Type
+     * @return converted map
+     */
+    public static <Source, Key, Value> Map<Key, Value> toMap(List<Source> sourceList, Function<Source, Key> keyMapper, Function<Source, Value> valueMapper) {
+        return sourceList.stream().collect(Collectors.toMap(keyMapper, valueMapper));
+    }
+
+    /**
+     * Illustration of list joining with prefix and suffix
+     *
+     * @param sourceList list of {@link Integer}
+     * @return
+     * @see <a href="https://www.baeldung.com/java-8-collectors">Guide to Java 8’s Collectors</a>
+     */
+    public static String toJsonNumberArray(List<Integer> sourceList) {
+        return sourceList.stream().map(String::valueOf).collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    /**
+     * Illustration of list joining with prefix and suffix
+     *
+     * @param sourceList list of {@link String}
+     * @return
+     * @see <a href="https://www.baeldung.com/java-8-collectors">Guide to Java 8’s Collectors</a>
+     */
+    public static String toJsonStringArray(List<String> sourceList) {
+        return sourceList.stream().map(word -> "\"" + word + "\"").collect(Collectors.joining(", ", "[", "]"));
+    }
+}
---
+++ libraries/library-utils/src/test/java/com/github/frtu/utils/stream/GroupConverterTest.java
@@ -0,0 +1,112 @@
+package com.github.frtu.utils.stream;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@Slf4j
+public class GroupConverterTest {
+    @Test
+    public void toMapByKey() {
+        //--------------------------------------
+        // 1. Prepare data
+        //--------------------------------------
+        List<String> wordList = Arrays.asList("alphabet", "ballon", "cat", "dog");
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        // Dict of first letter
+        final Map<Character, String> characterStringMap = GroupConverter.toMapByKey(wordList, word -> word.charAt(0));
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        assertEquals("alphabet", characterStringMap.get('a'));
+        assertEquals("ballon", characterStringMap.get('b'));
+        assertEquals("cat", characterStringMap.get('c'));
+        assertEquals("dog", characterStringMap.get('d'));
+    }
+
+    @Test
+    public void toMapByValue() {
+        //--------------------------------------
+        // 1. Prepare data
+        //--------------------------------------
+        List<String> wordList = Arrays.asList("alphabet", "ballon", "cat", "dog");
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        final Map<String, Integer> characterStringMap = GroupConverter.toMapByValue(wordList, String::length);
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        assertEquals((Integer) 8, characterStringMap.get("alphabet"));
+        assertEquals((Integer) 6, characterStringMap.get("ballon"));
+        assertEquals((Integer) 3, characterStringMap.get("cat"));
+        assertEquals((Integer) 3, characterStringMap.get("dog"));
+    }
+
+    @Test
+    public void toJsonNumberArray() throws IOException {
+        //--------------------------------------
+        // 1. Prepare data
+        //--------------------------------------
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4);
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        final String json = GroupConverter.toJsonNumberArray(numberList);
+        LOGGER.debug(json);
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        ObjectMapper objectMapper = new ObjectMapper();
+        final List<String> resultList = objectMapper
+                .readValue(json, new TypeReference<List<Integer>>() {
+                });
+
+        assertEquals(numberList.size(), resultList.size());
+        LOGGER.debug("First item:{}, type:{}", resultList.get(0));
+        assertEquals(1, resultList.get(0));
+    }
+
+    @Test
+    public void toJsonStringArray() throws IOException {
+        //--------------------------------------
+        // 1. Prepare data
+        //--------------------------------------
+        List<String> wordList = Arrays.asList("alphabet", "ballon", "cat", "dog");
+
+        //--------------------------------------
+        // 2. Execute
+        //--------------------------------------
+        final String json = GroupConverter.toJsonStringArray(wordList);
+        LOGGER.debug(json);
+
+        //--------------------------------------
+        // 3. Validate
+        //--------------------------------------
+        ObjectMapper objectMapper = new ObjectMapper();
+        final List<String> resultList = objectMapper
+                .readValue(json, new TypeReference<List<String>>() {
+                });
+        assertEquals(wordList.size(), resultList.size());
+        assertEquals("alphabet", resultList.get(0));
+        assertEquals("ballon", resultList.get(1));
+        assertEquals("cat", resultList.get(2));
+        assertEquals("dog", resultList.get(3));
+    }
+}
