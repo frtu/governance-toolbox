73ba78e1749f5f52338ae4802ddf9caa5680dcb5
Tu, Fred <frederic.tu@gmail.com>
2020-12-26 21:57:52 +0800

    Fix DMI_COLLECTION_OF_URLS

--- libraries/library-spring-reflection/src/main/java/com/github/frtu/spring/reflect/ClassloaderUtil.java
+++ libraries/library-spring-reflection/src/main/java/com/github/frtu/spring/reflect/ClassloaderUtil.java
@@ -4,11 +4,13 @@ import org.springframework.util.ClassUtils;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Frédéric TU
@@ -19,23 +21,28 @@ public class ClassloaderUtil {
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ClassloaderUtil.class);
 
     public static void reloadClassloader(List<String> classpathElements) {
-        try {
-            Set<URL> urls = new HashSet<URL>();
-            for (String element : classpathElements) {
-                URL url = new File(element).toURI().toURL();
-                urls.add(url);
-                logger.debug("Add to url list:{}", urls);
-            }
-
-            logger.info("All urls to the Thread.currentThread().setContextClassLoader()");
-            // ClassLoader currentContextClassLoader = Thread.currentThread().getContextClassLoader();
-            ClassLoader currentContextClassLoader = ClassUtils.getDefaultClassLoader();
-            ClassLoader newContextClassLoader = URLClassLoader.newInstance(urls.toArray(new URL[0]), currentContextClassLoader);
-
-            // Thread.currentThread().setContextClassLoader(newContextClassLoader);
-            ClassUtils.overrideThreadContextClassLoader(newContextClassLoader);
-        } catch (MalformedURLException e) {
-            throw new IllegalArgumentException(e);
+        Set<URI> uris = new HashSet<URI>();
+        for (String element : classpathElements) {
+            final URI uri = new File(element).toURI();
+            uris.add(uri);
+            logger.debug("Add to url list:{}", uris);
         }
+
+        logger.info("All uris to the Thread.currentThread().setContextClassLoader()");
+        // ClassLoader currentContextClassLoader = Thread.currentThread().getContextClassLoader();
+        ClassLoader currentContextClassLoader = ClassUtils.getDefaultClassLoader();
+
+
+        final List<URL> urlList = uris.stream().map(uri -> {
+            try {
+                return uri.toURL();
+            } catch (MalformedURLException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }).collect(Collectors.toList());
+        ClassLoader newContextClassLoader = URLClassLoader.newInstance(urlList.toArray(new URL[urlList.size()]), currentContextClassLoader);
+
+        // Thread.currentThread().setContextClassLoader(newContextClassLoader);
+        ClassUtils.overrideThreadContextClassLoader(newContextClassLoader);
     }
 }
